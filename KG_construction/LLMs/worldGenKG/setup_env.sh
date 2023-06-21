880f7e5e5aaeddd143826be8526a3b43d24eb671
zedd-08 <manish.96@live.com>
2023-06-21 20:37:08 +0200

    AskBERT code, taken from https://github.com/rajammanabrolu/WorldGeneration

---
+++ KG_construction/LLMs/worldGenKG/setup_env.sh
@@ -0,0 +1,4 @@
+conda create env -f environment.yml 
+conda activate askBert 
+conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
+pip install -r requirements.txt
