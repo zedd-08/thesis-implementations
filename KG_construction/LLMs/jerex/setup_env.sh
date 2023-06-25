1cff996727566098d442eaead3b031b8559f4a01
zedd-08 <manish.96@live.com>
2023-06-25 17:02:11 +0200

    working dependency states

--- KG_construction/LLMs/jerex/setup_env.sh
+++ KG_construction/LLMs/jerex/setup_env.sh
@@ -1,4 +1,4 @@
 conda create -n jerex python=3.7
 conda activate jerex
-conda install -y pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=11.3 -c pytorch -c conda-forge
+pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 torchtext==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 yes | pip install -r requirements.txt
