2401b1aa12d765bb547024bb9395ae02e5713d7f
zedd-08 <manish.96@live.com>
2023-07-10 17:06:20 +0200

    Trying out AskBERT model

--- KG_construction/LLMs/worldGenKG/setup_env.sh
+++ KG_construction/LLMs/worldGenKG/setup_env.sh
@@ -1,4 +1,4 @@
-conda env create -y -f environment.yml 
+conda env create -f environment.yml 
 conda activate askBert 
 conda install -y pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
 echo 'y' | pip install -r requirements.txt
--- KG_construction/LLMs/worldGenKG/write_graph_to_png.py
+++ KG_construction/LLMs/worldGenKG/write_graph_to_png.py
@@ -1,6 +1,10 @@
 import pydot
 import sys
 
+if len(sys.argv)!=3:
+	sys.exit(f'Usage: python write_graph_to_png.py $inp_dot $out_png')
+	
+
 inp_file = sys.argv[1]
 out_file = sys.argv[2]
 
