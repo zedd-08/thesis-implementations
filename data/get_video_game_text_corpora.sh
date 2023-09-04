7d6a128f4d5c149bbea0a7058d3e59c9c7f4e720
zedd-08 <manish.96@live.com>
2023-09-04 14:52:38 +0200

    Updated code to download some datasets

--- data/get_video_game_text_corpora.sh
+++ data/get_video_game_text_corpora.sh
@@ -1,4 +1,10 @@
-wget https://jakub.thebias.nl/GPT2_WOWHead_dataset.txt
+echo "Fetching scraped WOWHead dataset, by Jakub Mysliwiec"
+wget -O wow_quests_dataset.txt https://jakub.thebias.nl/GPT2_WOWHead_dataset.txt
+
+echo "Fetching Video game text corpora by Judith van Stegeren et al."
 git clone git@github.com:hmi-utwente/video-game-text-corpora.git
-cd video-game-text-corpora
-rm -rf .git
+cd video-game-text-corpora && rm -rf .git && cd ../
+
+echo "Fetching GPT2 Quest generation dataset by Sam Vartinen et al."
+git clone git@github.com:svartinen/gpt2-quest-descriptions.git VartinenQuests
+cd VartinenQuests && rm -rf .git && cd ../
